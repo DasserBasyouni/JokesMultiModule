3eef9d948967224dd07069f35cc4b3f682b89b5a
dasserbasyouni <dasserbasyouni@gmail.com>
2019-03-31 15:55:47 +0200

    fix: fixed paid version crash

--- app/src/paid/java/com/example/dasser/jokesmultimodule/MainActivity.java
+++ app/src/paid/java/com/example/dasser/jokesmultimodule/MainActivity.java
@@ -2,12 +2,12 @@ package com.example.dasser.jokesmultimodule;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
